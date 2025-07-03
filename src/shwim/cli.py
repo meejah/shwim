61ddae8d7d71a5808f0a046f63781c65adef3c60
meejah <meejah@meejah.ca>
2025-07-03 00:17:30 -0600

    appears to work

--- src/shwim/cli.py
+++ src/shwim/cli.py
@@ -5,6 +5,8 @@ import termios
 import click
 import wormhole
 from fowl.api import create_coop
+from fowl._proto import create_fowl
+from fowl.status import _StatusTracker
 from fowl.observer import When
 from fowl.tcp import allocate_tcp_port
 from twisted.internet.defer import ensureDeferred, Deferred
@@ -21,13 +23,20 @@ from twisted.internet.error import ProcessDone
     help="The Mailbox URL to use",
     required=False,
 )
+@click.option(
+    "--read-only",
+    "-R",
+    help="Peers cannot provide input to the terminal",
+    flag_value="read_only",
+    default=False,
+)
 @click.argument("code", default=None, required=False)
-def shwim(code, mailbox):
+def shwim(code, mailbox, read_only):
     """
     """
     if code is None:
         react(
-            lambda r: ensureDeferred(_host(r, mailbox))
+            lambda r: ensureDeferred(_host(r, mailbox, read_only))
         )
     else:
         react(
@@ -37,14 +46,15 @@ def shwim(code, mailbox):
 
 async def _guest(reactor, mailbox, code):
     wh = wormhole.create("meejah.ca/shwim", mailbox, reactor, dilation=True)
-    coop = create_coop(reactor, wh)
+    status = _StatusTracker()  # FIXME, we shouldn't have to fix this?
+    coop = create_coop(reactor, wh, status)
+
     wh.set_code(code)
     c = await wh.get_code()
     print("code", c)
 
     dilated = await coop.dilate()
     print("dilated")
-    coop._set_ready()
 
     x = coop.roost("tty-share")
     print("roosted", x)
@@ -52,9 +62,6 @@ async def _guest(reactor, mailbox, code):
     print(f"peer connected: {channel}")
     port = channel.connect_port
 
-    for x in range(4):
-        await deferLater(reactor, 1.0, lambda: None) 
-        print(".")
 
     await launch_tty_share(reactor, f"http://localhost:{port}/s/local/")
     await Deferred()
@@ -75,6 +82,15 @@ class TtyShare(Protocol):
         # we need to make some terminal Raw somewhere, how about here?
         self._origstate = termios.tcgetattr(0)
         tty.setraw(0)
+        self._sync_terminal_size()
+
+    def _sync_terminal_size(self):
+        # we should also sync terminal size on SIGWINCH I believe?
+        size = termios.tcgetwinsize(0)
+        print(self.transport)
+        print(dir(self.transport))
+        print(self.transport.fileno())
+        termios.tcsetwinsize(self.transport.fileno(), size)
 
     def childDataReceived(self, fd, data):
         #print(fd, data)
@@ -124,19 +140,29 @@ async def launch_tty_share(reactor, *args):
     await proto.when_done()
 
 
-async def _host(reactor, mailbox):
+async def _host(reactor, mailbox, read_only):
     wh = wormhole.create("meejah.ca/shwim", mailbox, reactor, dilation=True)
-    coop = create_coop(reactor, wh)
+    status = _StatusTracker()  # FIXME, we shouldn't have to fix this?
+    coop = create_coop(reactor, wh, status)
     wh.allocate_code()
     code = await wh.get_code()
     print(f"magic code: {code}")
 
     dilated = await coop.dilate()
-    print("peer connected")
-    coop._set_ready()
+    print("host: dilated")
 
-    # we're running the server
-    channel = await coop.fledge("tty-share", 5666, 5666)
+    # we're running the server -- we want a random port, but also we
+    # _NEED_ to have the same port in use on the far side, for boring
+    # HTTP reasons (the "same origin" check includes the port, so
+    # "localhost:1234" is not the same origin as "localhost:<other port>")
+    random_port = 8001###allocate_tcp_port()
+    # race between here, and when we acutally listen...
+    print("host: fledging")
+    channel = await coop.fledge("tty-share", random_port)#, random_port)
     print(f"running tty-share on: {channel.listen_port}")
 
-    await launch_tty_share(reactor, "--listen", f"localhost:{channel.listen_port}")
+    if 0:
+        await Deferred()
+    else:
+        ro_args = ["-readonly"] if read_only else []
+        await launch_tty_share(reactor, "--listen", f"localhost:{channel.listen_port}", *ro_args)
